06f7f12e5531d5669ad7a1470101482e6b7ce577
Wang Hao <wh.sjtu@gmail.com>
2012-10-26 14:57:24 +0800

    lab_03 start

---
+++ lab_03/yfs_client.h
@@ -0,0 +1,59 @@
+#ifndef yfs_client_h
+#define yfs_client_h
+
+#include <string>
+//#include "yfs_protocol.h"
+#include "extent_client.h"
+#include <vector>
+
+#include "lock_protocol.h"
+#include "lock_client.h"
+
+class yfs_client {
+	extent_client *ec;
+	public:
+
+	typedef unsigned long long inum;
+	enum xxstatus { OK, RPCERR, NOENT, IOERR, EXIST };
+	typedef int status;
+
+	struct fileinfo {
+		unsigned long long size;
+		unsigned long atime;
+		unsigned long mtime;
+		unsigned long ctime;
+	};
+	struct dirinfo {
+		unsigned long atime;
+		unsigned long mtime;
+		unsigned long ctime;
+	};
+	struct dirent {
+		std::string name;
+		yfs_client::inum inum;
+	};
+
+	private:
+	static std::string filename(inum);
+	static inum n2i(std::string);
+	inum gen_inum(bool);
+	void build_root();
+	
+	public:
+
+	yfs_client(std::string, std::string);
+
+	bool isfile(inum);
+	bool isdir(inum);
+	bool lookup(inum, const char*, inum &);
+
+	int getfile(inum, fileinfo &);
+	int getdir(inum, dirinfo &);
+	int create(inum, const char*, inum*);
+	int read_dir(inum, std::list<struct yfs_client::dirent> &);
+	int write(inum, size_t, off_t, const char *);
+	int read(inum, size_t, off_t, std::string &);
+	int setattr(inum, long long int);
+};
+
+#endif 
