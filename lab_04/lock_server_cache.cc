c10fd63cd2d51ec5721f28cdc2e3c9811dd13a20
Wang Hao <wh.sjtu@gmail.com>
2012-11-07 16:17:49 +0800

    small stop

--- lab_04/lock_server_cache.cc
+++ lab_04/lock_server_cache.cc
@@ -9,20 +9,49 @@
 #include "handle.h"
 #include "tprintf.h"
 
-
 lock_server_cache::lock_server_cache()
 {
 	nacquire = 0;
-	
+	VERIFY(pthread_mutex_init(&mutex, NULL) == 0);
 }
+
+lock_server_cache::~lock_server_cache()
+{
+	VERIFY(pthread_mutex_destroy(&mutex) == 0);
+}
+
 
 int lock_server_cache::acquire(lock_protocol::lockid_t lid, std::string id, 
 		int &)
 {
-	lock_protocol::status ret = lock_protocol::OK;
-	printf("acquire request from clt %s for lock %016llx\n", id, lid);
+	lock_protocol::status ret;
+	printf("acquire request from clt %s for lock %016llx\n", id.c_str(), lid);
+	
+	//check the status of lock
+	pthread_mutex_lock(&mutex);
 
-	return ret;
+	if(l_map.find(lid) == l_map.end()) {
+	//lock is requested first time
+		std::list<std::string> wait_list;
+		wait_list.push_back(id);
+		l_map[lid] = wait_list;
+		
+		ret = lock_protocol::OK;
+		pthread_mutex_unlock(&mutex);
+		return ret;
+
+	} else {
+	//lock is on a LOCKED client
+		l_map[lid].push_back(id);
+		//send retry to the last one		
+		ret = lock_protocol::RETRY;
+		
+		pthread_mutex_unlock(&mutex);
+		
+		//send revoke to the first client
+		revoke(id, lid);
+		return ret;
+	}
 }
 
 int 
@@ -41,15 +70,31 @@ lock_server_cache::stat(lock_protocol::lockid_t lid, int &r)
 	return lock_protocol::OK;
 }
 
-int lock_server_cache::revoke(std::string)
+int lock_server_cache::revoke(std::string id, lock_protocol::lockid_t lid)
 {
+	//bind the client
+	handle h(id);
+	rpcc *cl = h.safebind();
+	if(cl) {
+		ret = cl->call(rlock_protocol::revoke);
+	} else {
+		printf("revoke rpc faild [client:%s, lock %016llx]\n", id.c_str(), lid);
+	}
 
+	return 0;
 }
 
-int lock_server_cache::retry(std::string)
+int lock_server_cache::retry(std::string id, lock_protocol::lockid_t lid)
 {
+	handle h(id);
+	rpcc *cl = h.safebind();
+	if(cl) {
+		ret = cl->call(rlock_protocol::retry);
+	} else {
+		printf("retry rpc faild [client:%s, lock %016llx]\n", id.c_str(), lid);
+	}
 
-
+	return 0;
 }
 
 
--- lab_04/lock_server_cache.h
+++ lab_04/lock_server_cache.h
@@ -7,24 +7,21 @@
 #include "lock_protocol.h"
 #include "rpc.h"
 #include "lock_server.h"
+#include "pthread.h"
 
 
 class lock_server_cache {
 	private:
 		int nacquire;
-		struct lock_rec {
-			unsigned long long lid;
-			int stat;
-			std::string h_cid;
-			std::list<std::string> wait_list;
-		};
-		
-		std::list<struct lock_rec> l_list;
-		int revoke(std::string id);
-		int retry(std::string id);
+		std::map<lock_protocol::lockid_t, std::list<std::string> > lock_map;
+		int revoke(std::string id, lock_protocol::lockid_t lid);
+		int retry(std::string id, lock_protocol::lockid_t lid);
+
+		pthread_mutex_t mutex;
 
 	public:
 		lock_server_cache();
+		~lock_server_cache();
 		lock_protocol::status stat(lock_protocol::lockid_t, int &);
 		int acquire(lock_protocol::lockid_t, std::string id, int &);
 		int release(lock_protocol::lockid_t, std::string id, int &);
